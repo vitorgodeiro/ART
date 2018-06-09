8188868816472bea74d38c4f7cf01e9d36367759
vitorgodeiro <vitorgodeiro@live.com>
2018-06-09 09:45:48 -0300

    Add vector class

--- src/art.cpp
+++ src/art.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <omp.h>
 #include "util/image.cpp"
+#include "../include/util/vec3.h"
 
 int main (int argc, char* argv[]){
+    
     int nx = 200;
     int ny = 100;
 
@@ -14,14 +16,7 @@ int main (int argc, char* argv[]){
         int y = j + .5;
         for (int i = 0; i < nx; i++){
             int x = i + .5;
-            float r = float(x) / float(nx);
-            float g = float(y) / float(ny);
-            float b = 0.2;
-            
-            img(i, ny - j - 1)[0] = r;
-            img(i, ny - j - 1)[1] = g;
-            img(i, ny - j - 1)[2] = b;
-            
+            img(i, ny - j - 1) = Color3f(float(x) / float(nx), float(y) / float(ny), 0.2);
         }
     }
 
